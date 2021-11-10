7990c757bf9490b345849bd40390d09ea45d118f
Ararat Asatryan <ar.asatryan94@gmail.com>
2021-11-10 05:24:05 +0400

    fix(routes): minor changes

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -1,4 +1,6 @@
 module.exports = app => {
+  const mongoose = require('mongoose');
+  const db = require("../models/index.js");
   const userController = require("../controllers/user-controller.js");
   const emailController = require("../controllers/email-controller.js");
   const emailsController = require("../controllers/emails-controller.js");
@@ -9,7 +11,13 @@ module.exports = app => {
   router.get("/", userController.findAll);
 
   // Retrieve a single User with username
-  router.get("/:username", userController.findOne);
+  router.get("/:userName", userController.findOne);
+
+  router.post("/", (req, res) => {
+      const users = db.users;
+      //console.log(users);
+      res.send(users);
+    });
 
   // send an email to Single user:
   router.post("/send", emailController.sendSingleMail);
